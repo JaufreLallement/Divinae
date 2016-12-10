83a47d26bb6034ebe6e71485a98675c81878fb3a
Melkor00 <lallement.jaufre@orange.fr>
2016-12-10 13:01:17 +0100

    Edit: modification des classes CarteCroyant et CarteGuideSpirituel -> Add: ajout des fonctionnalités suivantes: méthode permettant d'ajouter un croyant à un guide, méthode permettant d'ajouter les points de prieres des croyants d'un guide à une divinite.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a3/60727da6c6be00161948d447f33aa7df
@@ -0,0 +1,25 @@
+package cartes.cartes_action.cartes_action_effet.cartes_action_dogmatiques.guides_spirituels;
+import cartes.cartes_action.cartes_action_effet.cartes_action_dogmatiques.CarteActionDogmatique;
+
+/**
+ * 
+ * @author alexis
+ * La classe CarteGuideSpirituel décrit le comportement général de la carte GuideSpirituel
+ */
+
+public class CarteGuideSpirituel extends CarteActionDogmatique {
+
+	/* ---------- Attributs ---------- */
+	/* ---------- Constructeurs ---------- */
+	/* ---------- Getters & Setters ---------- */
+	/* ---------- Méthodes ---------- */
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Pandocreon Divinae/src/cartes/cartes_action/cartes_action_effet/carte_deus_ex/CarteDeusEx.java
+++ Pandocreon Divinae/src/cartes/cartes_action/cartes_action_effet/carte_deus_ex/CarteDeusEx.java
@@ -1,14 +1,17 @@
 package cartes.cartes_action.cartes_action_effet.carte_deus_ex;
+
+import cartes.cartes_action.cartes_action_effet.CartesActionEffet;
+
 /**
  * 
  * @author alexis
- * La classe CarteDeusEx dÃ©crit le comportement gÃ©nÃ©ral de la carte Deus Ex
+ * La classe CarteDeusEx décrit le comportement général des cartes Deus Ex
  */
-public class CarteDeusEx {
+public class CarteDeusEx extends CartesActionEffet{
 	/* ---------- Attributs ---------- */
 	/* ---------- Constructeurs ---------- */
 	/* ---------- Getters & Setters ---------- */
-	/* ---------- MÃ©thodes ---------- */
+	/* ---------- Méthodes ---------- */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
